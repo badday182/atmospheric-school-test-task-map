36fd4768f4ee90dd27db8dfdecc9a073fb67bdba
Valerii <badday182@gmail.com>
2025-12-16 10:55:21 +0200

    Implement interactive map with Leaflet and marker clustering

--- src/component/Map copy.tsx
+++ src/component/Map copy.tsx
@@ -8,16 +8,22 @@ import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 function Map() {
   return (
     <>
+      {/* <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}> */}
       <MapContainer
         className="markercluster-map"
-        center={[51.0, 19.0]}
-        zoom={4}
-        maxZoom={18}
+        center={[49.8397, 24.0297]}
+        zoom={13}
+        scrollWheelZoom={false}
       >
         <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        {/* <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker> */}
         <MarkerClusterGroup>
           <Marker position={[49.8397, 24.0297]} />
           <Marker position={[52.2297, 21.0122]} />
@@ -25,7 +31,6 @@ function Map() {
         </MarkerClusterGroup>
         ;
       </MapContainer>
-      ;
     </>
   );
 }
